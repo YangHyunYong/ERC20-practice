bf8d218d1d59f812ce4f405967e04308f3c5d17b
YangHyunYong <43340172+YangHyunYong@users.noreply.github.com>
2024-05-24 15:19:00 +0900

    Transfer

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 import MetamaskButton from "./components/MetamaskButton";
 import Erc20Connect from "./components/Erc20Connect";
 import Balance from "./components/Balance";
+import Transfer from "./components/Transfer";
 
 const App = () => {
   const [signer, setSigner] = useState();
   const [contract, setContract] = useState();
   const [name, setName] = useState();
   const [symbol, setSymbol] = useState();
+  const [balance, setBalance] = useState(null);
 
   const getNameSymbol = async () => {
     try {
@@ -29,20 +31,24 @@ const App = () => {
 
   return (
     <div className="min-h-screen flex flex-col justify-start items-center py-16">
-      <MetamaskButton signer={signer} setSigner={setSigner}></MetamaskButton>
+      <MetamaskButton signer={signer} setSigner={setSigner} />
       {signer && (
         <div className="mt-16 flex flex-col gap-8 grow max-w-xl w-full">
           <div className="box-style text-center">
             0x18393DAce171F8bADF5d0b9E0316d548D1072cE5
           </div>
-          <Erc20Connect
-            setContract={setContract}
-            name={name}
-            signer={signer}
-          ></Erc20Connect>
-
+          <Erc20Connect name={name} signer={signer} setContract={setContract} />
           {name && (
-            <Balance name={name} symbol={symbol} contract={contract}></Balance>
+            <>
+              <Balance
+                name={name}
+                symbol={symbol}
+                contract={contract}
+                balance={balance}
+                setBalance={setBalance}
+              />
+              <Transfer name={name} symbol={symbol} contract={contract} />
+            </>
           )}
         </div>
       )}
